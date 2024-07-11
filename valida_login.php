3614bed65154ba98c50903eda17a52ae0b46586e
ivan <ivan-guita@hotmail.com>
2024-07-11 09:50:29 -0300

    recebendo dados do formulario usando superglobais

---
+++ valida_login.php
@@ -0,0 +1,17 @@
+
+<?php
+
+  /*
+   os arrays superglobais get e post são gerados de forma automatica
+   para que seja possivel pegar os dados submetidos pelo formelari a esse arquivo  
+  
+  */
+   print_r($_POST);
+   echo'<br>';
+
+   echo $_POST['email'];
+   echo"<br>";
+   echo $_POST['senha'];
+
+
+?>
